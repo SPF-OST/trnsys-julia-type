28a3766aa4819783de3caa823762fb840e1824eb
Damian Birchler <damian.birchler@ost.ch>
2024-10-01 12:42:10 +0200

    Add example from `jlrs`.

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::path::PathBuf;
+
+use jlrs::prelude::*;
+
+fn run_julia_script() {
+    let mut julia = Builder::new().start_local().expect("Could not init Julia");
+
+    // Include some custom code defined in MyModule.jl.
+    // This is safe because the included code doesn't do any strange things.
+    unsafe {
+        let path = PathBuf::from("MyModule.jl");
+        if path.exists() {
+            julia.include(path).expect("Could not include file");
+        } else {
+            julia
+                .include("src/MyModule.jl")
+                .expect("Could not include file");
+        }
+    }
+
+    // Create a scope, the closure provided to this method can use a `GcFrame` to ensure Julia
+    // data is not cleaned up by the GC while it's in use.
+    let result = julia
+        .with_stack(|mut stack| {
+            stack.scope(|mut frame| {
+                let dim = Value::new(&mut frame, 4isize);
+                let iters = Value::new(&mut frame, 1_000_000isize);
+
+                unsafe {
+                    Module::main(&frame)
+                        // the submodule doesn't have to be rooted because it's never reloaded.
+                        .submodule(&frame, "MyModule")?
+                        .as_managed()
+                        // the same holds true for the function: the module is never reloaded so it's
+                        // globally rooted
+                        .function(&frame, "complexfunc")?
+                        .as_managed()
+                        // Call the function with the two arguments it takes
+                        .call2(&mut frame, dim, iters)
+                        // If you don't want to use the exception, it can be converted to a `JlrsError`
+                        // In this case the error message will contain the message that calling
+                        // `display` in Julia would show
+                        .into_jlrs_result()?
+                        // The function that was called returns a `Float64`, which can be unboxed as `f64`
+                        .unbox::<f64>()
+                }
+            })
+        })
+        .expect("Result is an error");
+
+    println!("Result: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        run_julia_script()
+    }
+}
